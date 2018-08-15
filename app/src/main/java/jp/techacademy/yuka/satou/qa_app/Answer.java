e0870a0ba630c1a400fc42dba132fb6e83394e34
yuka1228 <sato-y@zenrin-tokai.co.jp>
2018-08-15 15:24:56 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/yuka/satou/qa_app/Answer.java
@@ -0,0 +1,33 @@
+package jp.techacademy.yuka.satou.qa_app;
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getmAnswerUid() {
+        return mAnswerUid;
+    }
+}
